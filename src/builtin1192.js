b5f450f3d64e7fdf4b0d983b7a4a1aa32883e7d0
armoldo30 <nkemp2421@gmail.com>
2026-09-09 19:18:15 -0400

    fix: use embedded 1.19.2 prerequisite index

--- src/builtin1192.js
+++ src/builtin1192.js
@@ -20,11 +20,10 @@ import r19 from './builtin1192raw/r19.js';
 import r20 from './builtin1192raw/r20.js';
 import r21 from './builtin1192raw/r21.js';
 import r22 from './builtin1192raw/r22.js';
-import REQUIREMENTS_1192 from './builtin1192raw/requirements.js';
 
 const text=[r01,r02,r03,r04,r05,r06,r07,r08,r09,r10,r11,r12,r13,r14,r15,r16,r17,r18,r19,r20,r21,r22].join('');
 export const BUILTIN_1192_LOADER_MODE='plain-json-modules';
 export const BUILTIN_1192=JSON.parse(text);
-BUILTIN_1192.requirements=REQUIREMENTS_1192;
-BUILTIN_1192.meta={...(BUILTIN_1192.meta||{}),requirementIndex:true,requirementRecords:679};
+const requirementRelationships=Object.values(BUILTIN_1192.requirements||{}).reduce((total,group)=>total+Object.values(group||{}).reduce((n,list)=>n+(Array.isArray(list)?list.length:0),0),0);
+BUILTIN_1192.meta={...(BUILTIN_1192.meta||{}),requirementIndex:true,requirementRecords:requirementRelationships};
 export default BUILTIN_1192;

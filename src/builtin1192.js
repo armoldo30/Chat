06aa6957f41be04d3184f826136180a0c2adf395
armoldo30 <nkemp2421@gmail.com>
2026-09-10 20:53:25 -0400

    audit: attach bounded Defines certification to built-in pack

--- src/builtin1192.js
+++ src/builtin1192.js
@@ -32,6 +32,7 @@ import technologySource1192 from './builtin1192/technology-source-manifest-1192.
 import doctrineSource1192 from './builtin1192/doctrine-source-manifest-1192.js';
 import doctrineSourceSupplement1192 from './builtin1192/doctrine-source-supplement-1192.js';
 import terrainTacticsModifiersCertification1192 from './builtin1192/terrain-tactics-modifiers-certification-1192.js';
+import { DEFINE_VALUES_1192 } from './builtin1192/defines-certification-1192.js';
 import technologyGraphIndustry1192 from './builtin1192/technology-graph-industry-1192.js';
 import technologyGraphSupport1192 from './builtin1192/technology-graph-support-1192.js';
 import technologyGraphInfantry1192 from './builtin1192/technology-graph-infantry-1192.js';
@@ -46,6 +47,12 @@ import { extractTechnologies, normalizeTechnologyGraph, normalizeCombatTacticRec
 const text=[r01,r02,r03,r04,r05,r06,r07,r08,r09,r10,r11,r12,r13,r14,r15,r16,r17,r18,r19,r20,r21,r22].join('');
 export const BUILTIN_1192_LOADER_MODE='plain-json-modules';
 export const BUILTIN_1192=JSON.parse(text);
+BUILTIN_1192.defines=BUILTIN_1192.defines||{};
+for(const [group,values] of Object.entries(DEFINE_VALUES_1192)){
+  const target=BUILTIN_1192.defines[group]||(BUILTIN_1192.defines[group]={});
+  for(const [key,value] of Object.entries(values))if(target[key]===undefined)target[key]=value;
+}
+BUILTIN_1192.meta={...(BUILTIN_1192.meta||{}),definesCertification:'bounded-corroborated-source',plannerConsumedDefineCount:24};
 for(const [id,record] of Object.entries(BUILTIN_1192.combatTactics||{}))BUILTIN_1192.combatTactics[id]=normalizeCombatTacticRecord(id,record?.raw||{});
 for(const [id,record] of Object.entries(BUILTIN_1192.modifiers||{}))BUILTIN_1192.modifiers[id]=normalizeModifierDefinitionRecord(id,record?.raw||{});
 const bundledDoctrineMetadata={};

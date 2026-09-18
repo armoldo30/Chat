13b650aaed1c5f0454820ef077629ef3666287db
armoldo30 <nkemp2421@gmail.com>
2026-09-17 23:47:43 -0400

    Bootstrap display labels from HOI4 1.19.3 localization

--- src/ui-localization-bootstrap.js
+++ src/ui-localization-bootstrap.js
@@ -1,6 +1,6 @@
 import { setDisplayLocalization } from './ui-labels.js';
 import { mergeLocalization } from './ui-localization.js';
-import BUILTIN_ENGLISH_LOCALIZATION_1192 from './builtin1192/localization-english-1192.js';
+import BUILTIN_ENGLISH_LOCALIZATION_1193 from './builtin1193/localization-english-1193.js';
 
 export const LOCALIZATION_STORAGE_KEY='hoi4-war-planner-localization-english-v1';
 
@@ -10,7 +10,7 @@ export function loadBrowserLocalization(storage=globalThis?.localStorage){
 }
 
 export function combinedDisplayLocalization(custom={}){
-  return mergeLocalization(BUILTIN_ENGLISH_LOCALIZATION_1192,custom);
+  return mergeLocalization(BUILTIN_ENGLISH_LOCALIZATION_1193,custom);
 }
 
 export function bootstrapDisplayLocalization(storage=globalThis?.localStorage){

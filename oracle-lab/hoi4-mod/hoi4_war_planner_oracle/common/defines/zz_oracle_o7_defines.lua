21fa8dc1fa0493eccb5273520ed241e7b8f50901
armoldo30 <nkemp2421@gmail.com>
2026-09-18 22:10:48 -0400

    Add O7 harness: zz_oracle_o7_defines.lua

---
+++ oracle-lab/hoi4-mod/hoi4_war_planner_oracle/common/defines/zz_oracle_o7_defines.lua
@@ -0,0 +1,8 @@
+-- HOI4 War Planner Oracle O7 diagnostic interventions.
+-- NOT vanilla balance data. Remove before ordinary Oracle scenarios.
+
+NDefines.NMilitary.BASE_CHANCE_TO_AVOID_HIT = 0
+NDefines.NMilitary.LAND_COMBAT_ORG_DICE_SIZE = 1
+NDefines.NMilitary.LAND_COMBAT_ORG_ARMOR_ON_SOFT_DICE_SIZE = 1
+NDefines.NMilitary.LAND_COMBAT_STR_DAMAGE_MODIFIER = 0
+NDefines.NMilitary.BASE_NIGHT_ATTACK_PENALTY = 0

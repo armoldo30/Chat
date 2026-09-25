ba56e2ef9b820e2c3478aaee6fc8d0699d90723d
armoldo30 <nkemp2421@gmail.com>
2026-09-25 00:18:07 -0400

    Predeclare O15 undefended-hit harness: zz_oracle_o15_defines.lua

---
+++ oracle-lab/hoi4-mod/hoi4_war_planner_oracle/common/defines/zz_oracle_o15_defines.lua
@@ -0,0 +1,8 @@
+-- HOI4 War Planner Oracle O15 normal undefended-hit probability probe.
+-- NOT vanilla balance data.
+NDefines.NMilitary.BASE_CHANCE_TO_AVOID_HIT = 100
+NDefines.NMilitary.CHANCE_TO_AVOID_HIT_AT_NO_DEF = 60
+NDefines.NMilitary.LAND_COMBAT_ORG_DICE_SIZE = 1
+NDefines.NMilitary.LAND_COMBAT_ORG_ARMOR_ON_SOFT_DICE_SIZE = 1
+NDefines.NMilitary.LAND_COMBAT_STR_DAMAGE_MODIFIER = 0
+NDefines.NMilitary.BASE_NIGHT_ATTACK_PENALTY = 0

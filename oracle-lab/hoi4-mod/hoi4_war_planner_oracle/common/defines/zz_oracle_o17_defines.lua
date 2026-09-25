2cd699a76dd073e585d5eddd5375e2619b0e2d55
armoldo30 <nkemp2421@gmail.com>
2026-09-25 00:51:47 -0400

    Predeclare O17 fixed strength-unit harness: zz_oracle_o17_defines.lua

---
+++ oracle-lab/hoi4-mod/hoi4_war_planner_oracle/common/defines/zz_oracle_o17_defines.lua
@@ -0,0 +1,11 @@
+-- HOI4 War Planner Oracle O17 fixed strength-damage unit calibration.
+-- NOT vanilla balance data.
+NDefines.NMilitary.BASE_CHANCE_TO_AVOID_HIT = 0
+NDefines.NMilitary.CHANCE_TO_AVOID_HIT_AT_NO_DEF = 100
+NDefines.NMilitary.LAND_COMBAT_ORG_DAMAGE_MODIFIER = 0
+NDefines.NMilitary.LAND_COMBAT_ORG_DICE_SIZE = 1
+NDefines.NMilitary.LAND_COMBAT_ORG_ARMOR_ON_SOFT_DICE_SIZE = 1
+NDefines.NMilitary.LAND_COMBAT_STR_DAMAGE_MODIFIER = 0.060
+NDefines.NMilitary.LAND_COMBAT_STR_DICE_SIZE = 1
+NDefines.NMilitary.LAND_COMBAT_STR_ARMOR_ON_SOFT_DICE_SIZE = 1
+NDefines.NMilitary.BASE_NIGHT_ATTACK_PENALTY = 0

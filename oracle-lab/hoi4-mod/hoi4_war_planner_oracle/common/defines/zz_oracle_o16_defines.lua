2eaec08ea168d81971b6860941f33c73ae732cb7
armoldo30 <nkemp2421@gmail.com>
2026-09-25 00:32:41 -0400

    Predeclare O16 organization-die harness: zz_oracle_o16_defines.lua

---
+++ oracle-lab/hoi4-mod/hoi4_war_planner_oracle/common/defines/zz_oracle_o16_defines.lua
@@ -0,0 +1,9 @@
+-- HOI4 War Planner Oracle O16 normal organization-die probe.
+-- NOT vanilla balance data.
+NDefines.NMilitary.BASE_CHANCE_TO_AVOID_HIT = 0
+NDefines.NMilitary.CHANCE_TO_AVOID_HIT_AT_NO_DEF = 100
+NDefines.NMilitary.LAND_COMBAT_ORG_DAMAGE_MODIFIER = 0.053
+NDefines.NMilitary.LAND_COMBAT_ORG_DICE_SIZE = 4
+NDefines.NMilitary.LAND_COMBAT_ORG_ARMOR_ON_SOFT_DICE_SIZE = 6
+NDefines.NMilitary.LAND_COMBAT_STR_DAMAGE_MODIFIER = 0
+NDefines.NMilitary.BASE_NIGHT_ATTACK_PENALTY = 0
